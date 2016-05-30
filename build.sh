45bfbfde02992f061e942c63a0220f816df16b96
Michael Kuzmin <mkuzmin@gmail.com>
2016-05-31 00:11:25 +0300

    Build under Docker container

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
-echo "Building provider vsphere"
-go build -o terraform-provider-vsphere
-	
-if [ $? -ne 0 ]; then
-	echo ""
-    echo "ERROR: build failed"
-fi
+#!/bin/sh -eux
+
+VERSION=0.6-dev
+
+rm -rf bin
+GOOS=darwin  GOARCH=amd64 go build -o bin/macos/terraform-provider-vsphere
+GOOS=linux   GOARCH=amd64 go build -o bin/linux/terraform-provider-vsphere
+GOOS=windows GOARCH=amd64 go build -o bin/windows/terraform-provider-vsphere.exe
+
+tar czf bin/terraform-vsphere-$VERSION-macos.tar.gz  --directory=bin/macos terraform-provider-vsphere
+tar czf bin/terraform-vsphere-$VERSION-linux.tar.gz  --directory=bin/linux terraform-provider-vsphere
+zip     bin/terraform-vsphere-$VERSION-windows.zip   -j bin/windows/terraform-provider-vsphere.exe
