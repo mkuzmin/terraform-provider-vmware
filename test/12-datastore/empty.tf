519f5c351905ee4b9f80eca802d22e7e4df8d075
Michael Kuzmin <mkuzmin@gmail.com>
2016-12-19 04:55:47 +0300

    testcase for datastore

---
+++ test/12-datastore/empty.tf
@@ -0,0 +1,12 @@
+provider "vmware" {
+  vcenter_server = "vcenter.vsphere5.test"
+  user = "root"
+  password = "jetbrains"
+  insecure_connection = true
+}
+resource "vmware_virtual_machine" "vm" {
+  name =  "vm-1"
+  image = "empty"
+  datastore = "datastore3"
+  power_on = false
+}
