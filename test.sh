b4d80cc1cdb28a63c38de693f05fbb9823b5f8e4
Michael Kuzmin <mkuzmin@gmail.com>
2017-09-02 13:06:15 +0300

    fix test env

--- test.sh
+++ test.sh
@@ -1,7 +1,9 @@
-#!/bin/bash -e
+#!/bin/sh
+
+set -e
 
 export TF_ACC=1
-export VSPHERE_SERVER=vcenter.vsphere.test
+export VSPHERE_SERVER=vcenter.vsphere55.test
 export VSPHERE_USER=root
 export VSPHERE_PASSWORD=jetbrains
 export VSPHERE_INSECURE=true
